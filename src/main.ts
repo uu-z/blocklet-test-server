b66ff2961546ad2c2c24ee26679491d2cf51a217
uuz <ncysatnaf@gmail.com>
2020-11-21 13:02:15 +0800

    blocklet bundle

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import passport from "@abtnode/passport";
 
-const { PORT = 3000 } = process.env;
+const { BLOCKLET_PORT = 3000 } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(PORT, () => Logger.log(`Server is running on http://localhost:${PORT}`));
+  app.use(
+    passport({
+      nodeHost: `http://127.0.0.1:${process.env.ABT_NODE_PORT}`,
+      blockletSk: process.env.BLOCKLET_APP_SK,
+      blockletRoutes: [/^\/admin/, /^\/config/]
+    })
+  );
+  await app.listen(BLOCKLET_PORT, () => Logger.log(`Server is running on http://localhost:${BLOCKLET_PORT}`));
 }
 bootstrap();
--- src/modules/forge/dto/update-forge.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateForgeDto } from './create-forge.dto';
-
-export class UpdateForgeDto extends PartialType(CreateForgeDto) {}
